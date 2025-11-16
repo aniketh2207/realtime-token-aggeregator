9aaa025374e025de054eecd101038f750e76779f
23f2001838 <23f2001838@ds.study.iitm.ac.in>
2025-11-16 14:02:27 +0000

    feat: resolving errors in redis environmental connection

--- src/services/cache.ts
+++ src/services/cache.ts
@@ -1,17 +1,18 @@
-import Redis from 'ioredis';
+import Redis from "ioredis";
 
-const connectionUrl = process.env.REDIS_URL || 'redis://localhost:6379';
-const redisClient = new Redis({
-    maxRetriesPerRequest: null,
-    enableReadyCheck: false,
+const connectionUrl = process.env.REDIS_URL || "redis://localhost:6379";
+
+const redisClient = new Redis(connectionUrl, {
+  maxRetriesPerRequest: null,
+  enableReadyCheck: false,
 });
 
-redisClient.on('connect',() =>{
-    console.log('Successfully connected to redis server');
+redisClient.on("connect", () => {
+  console.log("Successfully connected to redis server");
 });
 
-redisClient.on('error',(err) => {
- console.log('Could not connect to redis server: ', err);
+redisClient.on("error", (err) => {
+  console.log("Could not connect to redis server:", err);
 });
 
 export default redisClient;
